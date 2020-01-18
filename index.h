d6a82d15fb33b17768e57695ca3b9a756b886831
otti <github@ottersberg.name>
2020-01-18 15:45:41 +0100

    Main web page with live graph

---
+++ index.h
@@ -0,0 +1,85 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+  <style>
+    body {
+      min-width: 310px;
+    	max-width: 800px;
+    	height: 400px;
+      margin: 0 auto;
+    }
+    h2 {
+      font-family: Arial;
+      font-size: 2.5rem;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <h2>Growatt 1 kWp</h2>
+  <div id="chart-power" class="container"></div>
+  
+  <p id="AcPower"></p>
+  <p id="EnergyToday"></p>
+  <p id="EnergyTotal"></p>
+  <p id="DcVoltage"></p>
+  
+</body>
+<script>
+var chartT = new Highcharts.Chart({
+  chart:{ renderTo : 'chart-power' },
+  title: { text: 'Power' },
+  series: [{
+    showInLegend: false,
+    data: []
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    },
+    series: { color: '#059e8a' }
+  },
+  xAxis: { type: 'datetime',
+    dateTimeLabelFormats: { second: '%H:%M:%S' }
+  },
+  yAxis: {
+    title: { text: 'Power [W]' }
+  },
+  credits: { enabled: false }
+});
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() 
+  {
+    if (this.readyState == 4 && this.status == 200) 
+	{
+
+	  var obj = JSON.parse(this.responseText);
+	  document.getElementById("AcPower").innerHTML = "Power: " + obj.AcPower + " W"; 
+	  document.getElementById("EnergyToday").innerHTML = "Today: " + obj.EnergyToday + " kWh"; 
+	  document.getElementById("EnergyTotal").innerHTML = "Total: " + obj.EnergyTotal + " kWh"; 
+	  document.getElementById("DcVoltage").innerHTML = "DC Voltage: " + obj.DcVoltage + " V"; 
+	
+      var x = (new Date()).getTime(),
+          y = obj.AcPower;
+      console.log(this.responseText);
+      if(chartT.series[0].data.length > 40) {
+        chartT.series[0].addPoint([x, y], true, true, true);
+      } else {
+        chartT.series[0].addPoint([x, y], true, false, true);
+      }
+    }
+  };
+  xhttp.open("GET", "./status", true);
+  xhttp.send();
+}, 5000 ) ;
+
+</script>
+
+</html>
+
+
+)=====";
