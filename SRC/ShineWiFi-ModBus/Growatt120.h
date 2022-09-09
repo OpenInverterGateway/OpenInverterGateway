fb9f78688ad025ed15280f59c32d69a4b360eed9
Jan-André Zinser <zinserjan@gmail.com>
2022-09-09 12:12:26 +0200

    feat: support 2 mppt and single/three phase

--- SRC/ShineWiFi-ModBus/Growatt120.h
+++ SRC/ShineWiFi-ModBus/Growatt120.h
@@ -6,26 +6,44 @@
 #include "GrowattTypes.h"
 
 // Growatt modbus protocol version 1.20
-typedef enum {
+typedef enum
+{
     P120_I_STATUS = 0,
-    P120_DC_POWER,
-    P120_DC_VOLTAGE,
-    P120_DC_INPUT_CURRENT,
-    P120_AC_FREQUENCY,
-    P120_AC_VOLTAGE,
-    P120_AC_OUTPUT_CURRENT,
-    P120_AC_POWER,
+    P120_INPUT_POWER,
+    P120_PV1_VOLTAGE,
+    P120_PV1_INPUT_POWER,
+    P120_PV1_INPUT_CURRENT,
+    P120_PV2_VOLTAGE,
+    P120_PV2_INPUT_POWER,
+    P120_PV2_INPUT_CURRENT,
+    P120_OUTPUT_POWER,
+    P120_GRID_FREQUENCY,
+    P120_GRID_L1_VOLTAGE,
+    P120_GRID_L1_OUTPUT_CURRENT,
+    P120_GRID_L1_OUTPUT_POWER,
+    P120_GRID_L2_VOLTAGE,
+    P120_GRID_L2_OUTPUT_CURRENT,
+    P120_GRID_L2_OUTPUT_POWER,
+    P120_GRID_L3_VOLTAGE,
+    P120_GRID_L3_OUTPUT_CURRENT,
+    P120_GRID_L3_OUTPUT_POWER,
     P120_ENERGY_TODAY,
     P120_ENERGY_TOTAL,
-    P120_OPERATING_TIME,
-    P120_TEMPERATURE,
+    P120_WORK_TIME_TOTAL,
+    P120_PV1_ENERGY_TODAY,
+    P120_PV1_ENERGY_TOTAL,
+    P120_PV2_ENERGY_TODAY,
+    P120_PV2_ENERGY_TOTAL,
+    P120_PV_ENERGY_TOTAL,
+    P120_INVERTER_TEMPERATURE,
+    P120_INVERTER_IPM_TEMPERATURE,
 } eP120InputRegisters_t;
 
-
-typedef enum {
-    P120_OnOff, // Register 0
+typedef enum
+{
+    P120_OnOff,            // Register 0
     P120_CMD_MEMORY_STATE, // Register 2
-    P120_Active_P_Rate // Register 3
+    P120_Active_P_Rate     // Register 3
 } eP120HoldingRegisters_t;
 
 void init_growatt120(sProtocolDefinition_t &Protocol);
