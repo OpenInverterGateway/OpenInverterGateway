22c38a1ff2c1b4daf249b476d6e7ce28df7af69d
crasu <spam@crasu.de>
2022-12-29 21:11:08 +0100

    Introduce clang auto format (#69)
    * Introduce clang auto format
    * Reformat all code with clang
    
    Signed-off-by: crasu <spam@crasu.de>

--- SRC/ShineWiFi-ModBus/Growatt.h
+++ SRC/ShineWiFi-ModBus/Growatt.h
@@ -4,34 +4,34 @@
 #include "GrowattTypes.h"
 
 class Growatt {
-  public:
-    Growatt();
-    sProtocolDefinition_t _Protocol;
+ public:
+  Growatt();
+  sProtocolDefinition_t _Protocol;
 
-    void begin(Stream &serial);
-    void InitProtocol();
+  void begin(Stream& serial);
+  void InitProtocol();
 
-    bool ReadInputRegisters();
-    bool ReadHoldingRegisters();
-    bool ReadData();
-    eDevice_t GetWiFiStickType();
-    sGrowattModbusReg_t GetInputRegister(uint16_t reg);
-    sGrowattModbusReg_t GetHoldingRegister(uint16_t reg);
-    bool ReadInputReg(uint16_t adr, uint32_t* result);
-    bool ReadInputReg(uint16_t adr, uint16_t* result);
-    bool ReadHoldingReg(uint16_t adr, uint32_t* result);
-    bool ReadHoldingReg(uint16_t adr, uint16_t* result);
-    bool WriteHoldingReg(uint16_t adr, uint16_t value);
-    void CreateJson(char *Buffer, const char *MacAddress);
-    void CreateUIJson(char *Buffer);
-  private:
-    eDevice_t _eDevice;
-    bool _GotData;
-    uint32_t _PacketCnt;
+  bool ReadInputRegisters();
+  bool ReadHoldingRegisters();
+  bool ReadData();
+  eDevice_t GetWiFiStickType();
+  sGrowattModbusReg_t GetInputRegister(uint16_t reg);
+  sGrowattModbusReg_t GetHoldingRegister(uint16_t reg);
+  bool ReadInputReg(uint16_t adr, uint32_t* result);
+  bool ReadInputReg(uint16_t adr, uint16_t* result);
+  bool ReadHoldingReg(uint16_t adr, uint32_t* result);
+  bool ReadHoldingReg(uint16_t adr, uint16_t* result);
+  bool WriteHoldingReg(uint16_t adr, uint16_t value);
+  void CreateJson(char* Buffer, const char* MacAddress);
+  void CreateUIJson(char* Buffer);
 
-    eDevice_t _InitModbusCommunication();
-    static double _round2(double value);
+ private:
+  eDevice_t _eDevice;
+  bool _GotData;
+  uint32_t _PacketCnt;
 
+  eDevice_t _InitModbusCommunication();
+  static double _round2(double value);
 };
 
-#endif // _GROWATT_H_
+#endif  // _GROWATT_H_
--- SRC/ShineWiFi-ModBus/ShineMqtt.cpp
+++ SRC/ShineWiFi-ModBus/ShineMqtt.cpp
@@ -2,112 +2,97 @@
 #if MQTT_SUPPORTED == 1
 #include "PubSubClient.h"
 
-void ShineMqtt::mqttSetup(const MqttConfig &config)
-{
-    this->mqttconfig = config;
+void ShineMqtt::mqttSetup(const MqttConfig& config) {
+  this->mqttconfig = config;
 
-    uint16_t intPort = config.mqttport.toInt();
-    if (intPort == 0)
-        intPort = 1883;
+  uint16_t intPort = config.mqttport.toInt();
+  if (intPort == 0) intPort = 1883;
 
-    #if ENABLE_DEBUG_OUTPUT == 1
-        Serial.print(F("MqttServer: "));
-        Serial.println(this->mqttconfig.mqttserver);
-        Serial.print(F("MqttPort: "));
-        Serial.println(intPort);
-        Serial.print(F("MqttTopic: "));
-        Serial.println(this->mqttconfig.mqtttopic);
-    #endif
+#if ENABLE_DEBUG_OUTPUT == 1
+  Serial.print(F("MqttServer: "));
+  Serial.println(this->mqttconfig.mqttserver);
+  Serial.print(F("MqttPort: "));
+  Serial.println(intPort);
+  Serial.print(F("MqttTopic: "));
+  Serial.println(this->mqttconfig.mqtttopic);
+#endif
 
-    // make sure the packet size is set correctly in the library
-    this->mqttclient.setBufferSize(MQTT_MAX_PACKET_SIZE);
-    this->mqttclient.setServer(this->mqttconfig.mqttserver.c_str(), intPort);
+  // make sure the packet size is set correctly in the library
+  this->mqttclient.setBufferSize(MQTT_MAX_PACKET_SIZE);
+  this->mqttclient.setServer(this->mqttconfig.mqttserver.c_str(), intPort);
 }
 
-String ShineMqtt::getId()
-{
-    #ifdef ESP8266
-        uint64_t id = ESP.getChipId();
-    #elif ESP32
-        uint64_t id = ESP.getEfuseMac();
-    #endif
+String ShineMqtt::getId() {
+#ifdef ESP8266
+  uint64_t id = ESP.getChipId();
+#elif ESP32
+  uint64_t id = ESP.getEfuseMac();
+#endif
 
-    return String("Growatt"+id);
+  return String("Growatt" + id);
 }
-
 
 // -------------------------------------------------------
 // Check the Mqtt status and reconnect if necessary
 // -------------------------------------------------------
-bool ShineMqtt::mqttReconnect()
-{
-    if (this->mqttconfig.mqttserver.length() == 0)
-    {
-        //No server configured
-        return false;
-    }
-
-    if (WiFi.status() != WL_CONNECTED)
-        return false;
-
-    if (this->mqttclient.connected())
-        return true;
-
-    if (millis() - this->previousConnectTryMillis >= (5000))
-    {
-        #if ENABLE_DEBUG_OUTPUT == 1
-            Serial.print("MqttServer: "); Serial.println(this->mqttconfig.mqttserver);
-            Serial.print("MqttUser: "); Serial.println(this->mqttconfig.mqttuser);
-            Serial.print("MqttTopic: "); Serial.println(this->mqttconfig.mqtttopic);
-            Serial.print("Attempting MQTT connection...");
-        #endif
-
-        //Run only once every 5 seconds
-        this->previousConnectTryMillis = millis();
-        // Attempt to connect with last will
-        if (this->mqttclient.connect(
-                getId().c_str(),
-                   this->mqttconfig.mqttuser.c_str(),
-                   this->mqttconfig.mqttpwd.c_str(),
-                   this->mqttconfig.mqtttopic.c_str(),
-                   1,
-                   1,
-                   "{\"InverterStatus\": -1 }")
-                   )
-        {
-            #if ENABLE_DEBUG_OUTPUT == 1
-                Serial.println("connected");
-                return true;
-            #endif
-        }
-        else
-        {
-            #if ENABLE_DEBUG_OUTPUT == 1
-                Serial.print("failed, rc=");
-                Serial.print(this->mqttclient.state());
-                Serial.println(" try again in 5 seconds");
-            #endif
-            WEB_DEBUG_PRINT("MQTT Connect failed")
-            previousConnectTryMillis = millis();
-        }
-    }
+bool ShineMqtt::mqttReconnect() {
+  if (this->mqttconfig.mqttserver.length() == 0) {
+    // No server configured
     return false;
+  }
+
+  if (WiFi.status() != WL_CONNECTED) return false;
+
+  if (this->mqttclient.connected()) return true;
+
+  if (millis() - this->previousConnectTryMillis >= (5000)) {
+#if ENABLE_DEBUG_OUTPUT == 1
+    Serial.print("MqttServer: ");
+    Serial.println(this->mqttconfig.mqttserver);
+    Serial.print("MqttUser: ");
+    Serial.println(this->mqttconfig.mqttuser);
+    Serial.print("MqttTopic: ");
+    Serial.println(this->mqttconfig.mqtttopic);
+    Serial.print("Attempting MQTT connection...");
+#endif
+
+    // Run only once every 5 seconds
+    this->previousConnectTryMillis = millis();
+    // Attempt to connect with last will
+    if (this->mqttclient.connect(getId().c_str(),
+                                 this->mqttconfig.mqttuser.c_str(),
+                                 this->mqttconfig.mqttpwd.c_str(),
+                                 this->mqttconfig.mqtttopic.c_str(), 1, 1,
+                                 "{\"InverterStatus\": -1 }")) {
+#if ENABLE_DEBUG_OUTPUT == 1
+      Serial.println("connected");
+      return true;
+#endif
+    } else {
+#if ENABLE_DEBUG_OUTPUT == 1
+      Serial.print("failed, rc=");
+      Serial.print(this->mqttclient.state());
+      Serial.println(" try again in 5 seconds");
+#endif
+      WEB_DEBUG_PRINT("MQTT Connect failed")
+      previousConnectTryMillis = millis();
+    }
+  }
+  return false;
 }
 
-void ShineMqtt::mqttPublish(const String& JsonString)
-{
-    if (this->mqttclient.connected())
-        this->mqttclient.publish(this->mqttconfig.mqtttopic.c_str(), JsonString.c_str(), true);
+void ShineMqtt::mqttPublish(const String& JsonString) {
+  if (this->mqttclient.connected())
+    this->mqttclient.publish(this->mqttconfig.mqtttopic.c_str(),
+                             JsonString.c_str(), true);
 }
 
 void ShineMqtt::updateMqttLed() {
-    if (!this->mqttclient.connected())
-        digitalWrite(LED_RT, 1);
-    else
-        digitalWrite(LED_RT, 0);
+  if (!this->mqttclient.connected())
+    digitalWrite(LED_RT, 1);
+  else
+    digitalWrite(LED_RT, 0);
 }
 
-void ShineMqtt::loop() {
-    this->mqttclient.loop();
-}
+void ShineMqtt::loop() { this->mqttclient.loop(); }
 #endif
