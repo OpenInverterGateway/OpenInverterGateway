4c8d9ef128e32f0bd6eb5c6ad3d8fdee016b9536
crasu <spam@crasu.de>
2023-01-17 22:01:45 +0100

    Only enable mqtts on esp32

--- SRC/ShineWiFi-ModBus/ShineWifi.h
+++ SRC/ShineWiFi-ModBus/ShineWifi.h
@@ -1,14 +1,13 @@
 #ifndef GROWATT_SHINEWIFI_S_SHINEWIFI_H
 #define GROWATT_SHINEWIFI_S_SHINEWIFI_H
 
-#ifdef MQTTS_BROKER_CA_CERT
-#define MQTTS_ENABLED 1
-#endif
-
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #elif ESP32
+#ifdef MQTTS_BROKER_CA_CERT
+#define MQTTS_ENABLED 1
+#endif
 #ifdef MQTTS_ENABLED
 #include <WiFiClientSecure.h>
 #else
