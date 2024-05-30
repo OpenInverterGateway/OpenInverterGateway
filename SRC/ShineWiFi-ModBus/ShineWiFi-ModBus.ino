0d3036f2e1e565b25a8998e89ccfa42f6285c1eb
Peter Lieven <pl@dlhnet.de>
2024-05-30 16:20:07 +0000

    fix: do not call sendMqttJson if mqtt is disabled
    we can disable mqtt be leaving the server address empty. In this case
    don't was CPU cycles and spoil the log.
    
    Signed-off-by: Peter Lieven <pl@dlhnet.de>

--- SRC/ShineWiFi-ModBus/ShineWiFi-ModBus.ino
+++ SRC/ShineWiFi-ModBus/ShineWiFi-ModBus.ino
@@ -742,7 +742,9 @@ void loop()
                     boolean mqttSuccess = false;
 
                     #if MQTT_SUPPORTED == 1
-                    mqttSuccess = sendMqttJson();
+                    if (shineMqtt.mqttEnabled()) {
+                        mqttSuccess = sendMqttJson();
+                    }
                     #endif
                     handleWdtReset(mqttSuccess);
 
