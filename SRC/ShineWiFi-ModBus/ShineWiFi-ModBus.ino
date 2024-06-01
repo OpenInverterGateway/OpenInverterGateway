75b87e0cc950bf05abc2f37e4048d6018da34702
Peter Lieven <pl@dlhnet.de>
2024-06-01 11:20:45 +0000

    feat: include hostname in json
    Signed-off-by: Peter Lieven <pl@dlhnet.de>

--- SRC/ShineWiFi-ModBus/ShineWiFi-ModBus.ino
+++ SRC/ShineWiFi-ModBus/ShineWiFi-ModBus.ino
@@ -469,7 +469,7 @@ void sendJson(ShineJsonDocument&  doc)
 void sendJsonSite(void)
 {
     StaticJsonDocument<JSON_DOCUMENT_SIZE> doc;
-    Inverter.CreateJson(doc, WiFi.macAddress());
+    Inverter.CreateJson(doc, WiFi.macAddress(), Config.hostname);
 
     sendJson(doc);
 }
@@ -503,7 +503,7 @@ boolean sendMqttJson(void)
 {
     StaticJsonDocument<JSON_DOCUMENT_SIZE> doc;
 
-    Inverter.CreateJson(doc, WiFi.macAddress());
+    Inverter.CreateJson(doc, WiFi.macAddress(), "");
     return shineMqtt.mqttPublish(doc);
 }
 #endif
