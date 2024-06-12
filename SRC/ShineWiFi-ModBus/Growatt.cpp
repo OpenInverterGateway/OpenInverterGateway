a09b81b29823a7a3a04aa29569b428624d5df854
Peter Lieven <pl@dlhnet.de>
2024-06-12 11:20:21 +0000

    fix(Growatt::camelCaseToSnakeCase): single uppercase at the of String
    this fixes the missing insertion of an underscore if the last
    character of the String is uppercase and the second last is lowercase.
    
    E.g. TemperatureA was not correctly converted to temperature_a before.
    
    Signed-off-by: Peter Lieven <pl@dlhnet.de>

--- SRC/ShineWiFi-ModBus/Growatt.cpp
+++ SRC/ShineWiFi-ModBus/Growatt.cpp
@@ -580,8 +580,9 @@ void Growatt::CreateUIJson(ShineJsonDocument& doc, String Hostname) {
 void Growatt::camelCaseToSnakeCase(String input, char* output) {
   int outputIndex = 0;
   for (uint i = 0; input[i] != '\0'; i++) {
-    if (i > 0 && i < input.length() - 1 && isUpperCase(input[i]) &&
-        (isLowerCase(input[i - 1]) || isLowerCase(input[i + 1]))) {
+    if (i > 0 && isUpperCase(input[i]) &&
+        (isLowerCase(input[i - 1]) ||
+         (i < input.length() - 1 && isLowerCase(input[i + 1])))) {
       output[outputIndex++] = '_';
     }
     output[outputIndex++] = toLowerCase(input[i]);
