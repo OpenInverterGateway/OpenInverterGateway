2cdf4c1dee0db6df8f41bc4627b72c5e0d96c866
Peter Lieven <pl@dlhnet.de>
2024-06-30 19:06:05 +0000

    refactor: convert remaing String to const String&
    fix remaining occasions where a String is used unaltered in a function and thus
    avoid duplication at function call.
    
    Signed-off-by: Peter Lieven <pl@dlhnet.de>

--- SRC/ShineWiFi-ModBus/Growatt.h
+++ SRC/ShineWiFi-ModBus/Growatt.h
@@ -30,8 +30,9 @@ class Growatt {
   bool ReadHoldingRegFrag(uint16_t adr, uint8_t size, uint32_t* result);
   bool WriteHoldingReg(uint16_t adr, uint16_t value);
   bool WriteHoldingRegFrag(uint16_t adr, uint8_t size, uint16_t* value);
-  void CreateJson(ShineJsonDocument& doc, String MacAddress, String Hostname);
-  void CreateUIJson(ShineJsonDocument& doc, String Hostname);
+  void CreateJson(ShineJsonDocument& doc, const String& MacAddress,
+                  const String& Hostname);
+  void CreateUIJson(ShineJsonDocument& doc, const String& Hostname);
   void CreateMetrics(StringStream& metrics, const String& MacAddress,
                      const String& Hostname);
 
@@ -44,7 +45,7 @@ class Growatt {
   eDevice_t _InitModbusCommunication();
   double roundByResolution(const double& value, const float& resolution);
   double getRegValue(sGrowattModbusReg_t* reg);
-  void camelCaseToSnakeCase(String input, char* output);
+  void camelCaseToSnakeCase(const String& input, char* output);
   void metricsAddValue(const String& name, double value, StringStream& metrics,
                        const String& labels);
   std::tuple<bool, String> handleEcho(const JsonDocument& req,
